7687cd80c1bc095ed7c285da2fb8c43be151569b
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-06 11:37:51 +0200

    Create HomeWork0509.java

---
+++ HomeWork0509.java
@@ -0,0 +1,12 @@
+class HomeWork0509 {
+    Public static void main(String[] args){
+    int x;
+    int y;
+    x = 5;
+    y = 3;
+    p = (x = y) * 2;
+    System.out.println(p);
+    
+}
+}
+    
