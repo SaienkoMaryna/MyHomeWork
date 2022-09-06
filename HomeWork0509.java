34d095387ee8795d56828ff534f3bc8a12a86bff
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-06 12:21:42 +0200

    Update HomeWork0509.java

--- HomeWork0509.java
+++ HomeWork0509.java
@@ -1,11 +1,21 @@
+/***
+*  MyHomeWork0509 version 06.09.2022
+*  Author MarynaSaienko 
+*
+*/
+
 class HomeWork0509 {
-    Public static void main(String[] args){
-    int x;
-    int y;
-    x = 5;
-    y = 3;
-    p = (x = y) * 2;
-    System.out.println(p);
+    public static void main(String[] args){
+        int a; // katet pryamougolnogo ravnobedrennogo treugolnika
+        int b; // gipotenuza pryamougolnogo treugolnika
+        a = 4;
+        b = 7;
+        int p = b+ a * 2;
+        int s = a * b / 2; // S=1/2*(a*h), t.e. ravna  polovine proizvedeniya gipotenuzy (osnovaniya) na vysotu.   
+                           // t.k. treugolnik pryamougolniy, to S=1/2*(a*b),t.k. katety perpendikulyarni i ravny,
+                           // to odin katet - eto visota, provedennaya ko vtoromu katetu.     
+        System.out.println(p);
+        System.out.println(s);
     
 }
 }
