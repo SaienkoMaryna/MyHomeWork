4f2d06a2246c23a6c836ee25dc9baefd13ffd699
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-10 00:18:55 +0200

    Create HomeWork0909.java

---
+++ HomeWork0909.java
@@ -0,0 +1,66 @@
+/***
+*  MyHomeWork0509 
+*
+*  @author MarynaSaienko 
+*  @todo 30.09.2022
+*  @date 09.09.2022
+*  
+*/
+class HomeWork0909 {
+    public static void main (String[] args) {
+        checkWeather();
+        canWalk();
+        int summa = add(8, 4);
+        System.out.println(summa);
+        int raznost = sub(8, 4);
+        System.out.println(raznost);
+        int delenie = del(8, 4);
+        System.out.println(delenie);
+        int umnozh = umn(8, 4);
+        System.out.println(umnozh);
+        } 
+                
+        // 1
+    static void checkWeather(){
+        int temperature = 39;
+        if (temperature > 0 && temperature <=18) {
+        System.out.println("It`s cold");
+        } else if (temperature < 0 ) {
+        System.out.println("It`s frost");
+        } else if (temperature >=19 && temperature <=28 ) {
+        System.out.println("It`s warm");
+        } else {
+        System.out.println("It`s Hot");
+        }
+    }
+        // 2
+         
+    static void canWalk(){    
+        boolean isWeekend = true;
+        boolean isRain = !false;
+        if (isWeekend && isRain ) {
+        System.out.println("You can walk");
+        } else {
+        System.out.println("You can`t walk");
+        }
+    }
+    
+        // 3
+    
+    static int add(int a, int b) {
+        int c = a + b;
+        return(c);
+    }  
+    static int sub(int a, int b) {
+        int d = a - b;
+        return(d);
+    }    
+    static int del(int a, int b) {
+        int x = a / b;
+        return(x);
+    }
+    static int umn(int a, int b) {
+        int e = a * b;
+        return(e);
+    } 
+    } 
