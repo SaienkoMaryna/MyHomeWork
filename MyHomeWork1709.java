0ad5a74dc643c2cbf415c8944639e653f2be6ef9
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-18 22:12:40 +0200

    Update MyHomeWork1709.java

--- MyHomeWork1709.java
+++ MyHomeWork1709.java
@@ -4,6 +4,39 @@ import java.util.Scanner;
 *  MyHomeWork1709 
 *
 *  @author MarynaSaienko 
-*  @todo 12.09.2022
-*  @date 14.09.2022
+*  @todo 14.09.2022
+*  @date 18.09.2022
 */
+class MyHomeWork1709  {
+    public static void main (String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = 0;
+        int quess = -1;
+        
+        do {
+        number = random.nextInt(10);               
+        for (int i = 0; i < 3; i++) {        
+            System.out.print(" quess the number [0..9] : " );
+            quess = scanner.nextInt();
+            if(quess < number) {
+                System.out.println("your number is less");
+            } else if (quess > number) {
+                System.out.println("your number is bigger");
+            } else {
+                System.out.println("your WIN , you quess my number"); 
+             break;                
+            }         
+        }      
+           
+            if(number != quess) {
+                System.out.println("your lose");   
+            }
+                System.out.println("repeat the game? yes - 1 , no  - 0 ");
+         
+        }  
+           while (scanner.nextInt() == 1);
+    }
+}
+      
