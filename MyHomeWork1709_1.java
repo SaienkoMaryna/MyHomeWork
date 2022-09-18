0162e38b5a3c7bda7490f80743f61bdaed4a6bcb
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-18 22:12:47 +0200

    Create MyHomeWork1709_1.java

---
+++ MyHomeWork1709_1.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+/**
+*  MyHomeWork1709 
+*
+*  @author MarynaSaienko 
+*  @todo 14.09.2022
+*  @date 18.09.2022
+*/
+class MyHomeWork1709_1  {
+    public static void main (String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
