f1e9d0bee4518f729bb2b25de92168d5355a5f1b
SaienkoMaryna <111528006+SaienkoMaryna@users.noreply.github.com>
2022-09-19 00:33:28 +0200

    Update MyHomeWork1709_1.java

--- MyHomeWork1709_1.java
+++ MyHomeWork1709_1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 /**
-*  MyHomeWork1709 
+*  MyHomeWork1709_1 
 *
 *  @author MarynaSaienko 
 *  @todo 14.09.2022
@@ -11,3 +11,24 @@ class MyHomeWork1709_1  {
     public static void main (String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        do{
+        System.out.println("You shoud thinkig of a number [0...10], if ready write : yes");
+        String otvet = scanner.next();
+        int number = random.nextInt(10);
+        System.out.println(" please, add the same number if ready write : yes");
+        otvet = scanner.next();
+        int result = number + number;
+        System.out.println(" please, divide by intention number if ready write : yes");
+        otvet = scanner.next();
+        int result2 = result / number;
+        System.out.println("i think that your number is 2.");
+        
+        if(result2 != 2) {
+                System.out.println("I am lose");   
+            }
+                System.out.println("repeat the game? yes - 1 or no  - 0 ");
+         
+        }  
+           while (scanner.nextInt() == 1 );
+    }
+}
